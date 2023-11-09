f7d4671f5fe81310acdfac0cfe333cf35d6a8b6c
masoud nikzad <masoud.niki79@gmail.coom>
2023-11-09 22:26:19 +0330

    transaction types sepreted to a table

--- app/Models/TransactionType.php
+++ app/Models/TransactionType.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionType extends Model
 {
     use HasFactory;
-    protected $table="";
+    protected $table="transaction_types";
+    protected $fillable=["name"];
 }
