d94d99ac2cccadb41aae56a96ec758fadbffb5f3
masoud nikzad <masoud.niki79@gmail.coom>
2023-11-09 22:13:00 +0330

    test for coupon exists validation added

--- tests/Feature/WalletControllerTest.php
+++ tests/Feature/WalletControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Testing\Fluent\AssertableJson;
@@ -13,7 +14,7 @@ class WalletControllerTest extends TestCase
     public function test_user_can_not_apply_coupon_when_username_does_not_exists(){
         $this
             ->postJson("/api/v1/wallet/applyCoupon",[
-                "username"=>"09011231111",
+                "username"=>"+989011111111",
                 "coupon_code"=>"tktkt",
             ])
             ->assertStatus(422)
@@ -21,7 +22,45 @@ class WalletControllerTest extends TestCase
                 $json
                     ->has("message")
                     ->has("errors")
-                    ->has("errors.username");
+                    ->has("errors.username",1)
+                    ->where("errors.username.0","The selected username is invalid.");
             });
     }
+    public function test_user_can_not_apply_coupon_when_username_does_not_match_with_regex(){
+        $this
+            ->postJson("/api/v1/wallet/applyCoupon",[
+                "username"=>"0901",
+                "coupon_code"=>"tktkt",
+            ])
+            ->assertStatus(422)
+            ->assertJson(function (AssertableJson $json){
+                $json
+                    ->has("message")
+                    ->has("errors")
+                    ->has("errors.username",1)
+                    ->where("errors.username.0","The username field format is invalid.");
+            });
+    }
+    public function test_user_can_not_apply_coupon_when_coupon_does_not_exists(){
+        //arrange
+        $user=User::factory()->create();
+        //act
+        $this
+            ->postJson("/api/v1/wallet/applyCoupon",[
+                "username"=>$user->username,
+                "coupon_code"=>"tktkt",
+            ])
+
+            //assertions
+            ->assertStatus(422)
+            ->assertJson(function (AssertableJson $json){
+                $json
+                    ->has("message")
+                    ->has("errors")
+                    ->has("errors.coupon_code",1)
+                    ->where("errors.coupon_code.0","The selected coupon code is invalid.");
+            });
+    }
+
+
 }
