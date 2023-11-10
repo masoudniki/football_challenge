5e299be0961027a751e512b7f015d822ddcb5d42
masoud nikzad <masoud.niki79@gmail.coom>
2023-11-10 15:07:19 +0330

    set new deffination for charge code according to migration

--- database/factories/ChargeCodeFactory.php
+++ database/factories/ChargeCodeFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ChargeCode>
  */
-class CouponCodeFactory extends Factory
+class ChargeCodeFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -18,7 +18,9 @@ class CouponCodeFactory extends Factory
     {
         return [
             "code"=>fake()->unique()->regexify("/[A-Za-z0-9]{20}/"),
-            "amount"=>fake()->randomDigitNotZero()
+            "amount"=>fake()->randomDigitNotZero(),
+            "limit_usage"=>fake()->randomDigit(),
+            "count_usage"=>0
         ];
     }
 }
